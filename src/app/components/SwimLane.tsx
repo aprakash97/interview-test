c79f0695632538c4d023f95ff0d6c5f442a32af9
Prakash Anandakumar <prakash.anandakumar@acentura.com>
2025-09-18 13:41:17 +0530

    base structure

--- src/app/components/SwimLane.tsx
+++ src/app/components/SwimLane.tsx
@@ -8,8 +8,8 @@ const SwimLane = ({
 }: SwimLaneProps) => {
     return (
         // <div className='w-[300px] min-h-screen bg-white border rounded shadow'>SwimLane</div>
-        <div className='flex-1 min-h-screen bg-white border border-r-0 last:border-r-0'>
-            <div className='w-full border-x-black h-10 bg-gray-600'>
+        <div className='flex-1 min-h-screen bg-white border border-r-0 border-gray-400 last:border-r-0'>
+            <div className='w-full border-b border-gray-400 h-10'>
                 {title}
             </div>
         </div>
